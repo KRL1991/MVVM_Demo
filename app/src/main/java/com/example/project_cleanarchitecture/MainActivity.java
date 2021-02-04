89228628e3af4d3e87b43b25fa0ac6454d0459af
KRL1991 <kimx4706@edu.easj.dk>
2021-02-04 13:44:43 +0100

    2 milepæl - dog virker textfield ikke endnu

--- app/src/main/java/com/example/project_cleanarchitecture/MainActivity.java
+++ app/src/main/java/com/example/project_cleanarchitecture/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.project_cleanarchitecture;
 
+import androidx.annotation.StringRes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        Button updateButton = (Button) findViewById(R.id.UpdateButton);
+        updateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText userInputEditText = (EditText) findViewById(R.id.UserInputEditText);
+                TextView savedTextTextView = (TextView) findViewById(R.id.SavedTextTextView);
+
+
+
+
+            }
+        });
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
     }
-}
