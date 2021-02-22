052be8c63cc948372221a0d4655cc8b0ace8e035
KRL1991 <kimx4706@edu.easj.dk>
2021-02-22 10:11:26 +0100

    3 milepæl nået created model, view, persitance and test package moved MainActivity into view package

--- app/src/main/java/view/MainActivity.java
+++ app/src/main/java/view/MainActivity.java
@@ -2,9 +2,7 @@ package view;
 
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
